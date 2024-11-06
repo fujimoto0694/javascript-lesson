b799edbd75cd54d4accdccabf3abb39072d328ae
藤本大輝 <dahuit133@gmail.com>
2024-11-06 22:57:26 +0900

    Q3.オブジェクト

--- question/app.js
+++ question/app.js
@@ -3,7 +3,6 @@ let age = nickname + '年齢は２８歳です。';
 console.log(age);
 
 
-
 let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
 let templateText = `私の好きな言語は` + languages[0] + `です。次は` + languages[3] + `を勉強してみたいです。`;
 console.log(templateText);
